d30cf035fcf4a05968c1ab5aa9f0b048ff5e2a2e
Willem-Jan Boogerd <willem-jan.boogerd@klm.com>
2017-01-24 11:22:59 +0100

    Added MySQL driver to installer script

--- bamboo-server.sh
+++ bamboo-server.sh
@@ -14,11 +14,17 @@ if [ -d $BAMBOO_DIR ]; then
   echo "-> Bamboo $BAMBOO_VERSION already found at $BAMBOO_DIR. Skipping download."
 else
   BAMBOO_TARBALL_URL=http://downloads.atlassian.com/software/bamboo/downloads/atlassian-bamboo-$BAMBOO_VERSION.tar.gz
+  MYSQLDRIVER_TARBALL_URL=https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.40.tar.gz
   echo "-> Downloading Bamboo $BAMBOO_VERSION from $BAMBOO_TARBALL_URL ..."
   wget --progress=dot:mega $BAMBOO_TARBALL_URL -O /tmp/atlassian-bamboo.tar.gz
+  echo "-> Downloading MySQL driver from $MYSQLDRIVER_TARBALL_URL ..."
+  wget --progress=dot:mega $MYSQLDRIVER_TARBALL_URL -O /tmp/mysql-connector-java.tar.gz
   echo "-> Extracting to $BAMBOO_DIR ..."
   tar xzf /tmp/atlassian-bamboo.tar.gz -C /opt
   rm -f /tmp/atlassian-bamboo.tar.gz
+  echo "-> Adding MySQL driver"
+  tar xzf /tmp/mysql-connector-java.tar.gz -C $BAMBOO_DIR/lib/
+  rm -f /tmp/mysql-connector-java.tar.gz
   echo "-> Installation completed"
 fi
 
